9ea9103b10c7152462ebf4423b2c8bbd5fa3dce5
Hu Andong <andong.hu@repugene.com>
2019-01-18 17:41:48 +0800

    liaokestudy

---
+++ Vue/vueDemo/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <test @test="test1">hehe1</test>
+    <test @test="test2">hehe2</test>
+  </div>
+</template>
+
+<script>
+  import Test from './test.vue'
+
+  export default {
+    name: 'HelloWorld',
+    components:{Test},
+    methods:{
+      test1(){
+        console.log(1)
+      },
+      test2(){
+        console.log(2)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ Vue/vueDemo/src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      shiwo
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        mounted(){
+          this.$emit('test')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+ a
